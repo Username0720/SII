44f50b355f5c4c01f4a65e4df85b083aaf11de7a
DeWay Waleev <WalleyWay@gmail.com>
2021-05-24 18:26:21 +0300

    first commit

---
+++ SII/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 524);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количесвто точек";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 522);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(31, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Начальная температура";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Коэффициент";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "0,5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 522);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(508, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 100);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод отжига";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(508, 108);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 221);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Муравьиный алгоритм";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(508, 335);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(239, 226);
+            this.textBox3.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 562);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Форм1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ SII/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SII
+{
+    public partial class Form1 : Form
+    {
+        public int points_count = 0;
+        public int count;
+        Point[] points = new Point[11];
+        public bool use_points = true;
+        Graphics g;
+        public Pen brush = new Pen(Color.Black, 2);
+        public Pen brush2 = new Pen(Color.Chocolate, 1);
+        public Pen brush3 = new Pen(Color.Blue, 1);
+        public double t, k, short_way;
+
+        
+        //кол-во точек на полотне, которое вы задаете
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)        
+        {
+            count = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            count = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)      //кнокпка очистить все
+        {
+            pictureBox1.Refresh();
+            use_points = true;
+            points_count = 0;
+            textBox3.Text = "";
+            Array.Clear(points, 0, points.Length);
+        }
+        
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)     //расстановка точек на полотне
+        {
+            if (use_points == true)
+            {
+                Point(e.X, e.Y);
+                points[points_count].X = e.X;
+                points[points_count].Y = e.Y;
+                points_count++;
+                if (points_count >= count) 
+                    use_points = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t = Convert.ToDouble(textBox1.Text);
+            k = Convert.ToDouble(textBox2.Text);
+            List<int[]> arList = new List<int[]>();
+            int[] track = Make_Track((int)numericUpDown1.Value);
+
+            points[track.Length - 1] = points[0];
+            track = change_Track(track);
+            arList.Add(track);
+            textBox3.Text += t + Environment.NewLine;
+            while (t > 1)
+            {
+                short_way = Milky_Way(track, points);
+                int[] new_track = new int[track.Length];
+                track.CopyTo(new_track, 0);
+                while (!check_List(arList, new_track))
+                    new_track = change_Track(new_track);
+                arList.Add(new_track);
+                if (Milky_Way(new_track, points) <= short_way)
+                {
+                    short_way = Milky_Way(new_track, points);
+                    track = new_track;
+                }
+                else
+                {
+                    if (checkTemper(Milky_Way(new_track, points) - short_way, t))
+                    {
+                        short_way = Milky_Way(new_track, points);
+                        track = new_track;
+                    }
+
+                }
+                t = k * t;
+                textBox3.Text += t + Environment.NewLine;
+            }
+
+            for (int i = 1; i < track.Length; i++)
+            {
+                g.DrawLine(brush2, points[track[i - 1]].X, points[track[i - 1]].Y, points[track[i]].X, points[track[i]].Y);
+            }
+
+            textBox3.Text += ("Метод отжига: " + Milky_Way(track, points) + Environment.NewLine);
+        }
+
+        //функции
+        #region functions
+        public static double Track(float x1, float y1, float x2, float y2)      //расстояниe м/у 2мя точками
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
+
+        private void Point(float x, float y)
+        {
+            g.DrawEllipse(brush, x, y, brush.Width, brush.Width);
+        }
+
+        public static int[] change_Track(int[] track)    //изменение последовательности точек
+        {
+            Random rnd = new Random();
+            int[] new_track = new int[track.Length];
+            track.CopyTo(new_track, 0);
+            int first = 0, second = 0;
+            while (first == second)
+            {
+                first = rnd.Next(1, track.Length - 1);
+                second = rnd.Next(1, track.Length - 1);
+            }
+            int cont = new_track[first];
+            new_track[first] = new_track[second];
+            new_track[second] = cont;
+            return new_track;
+        }
+
+        public static double Milky_Way(int[] track, Point[] points)      //весь путь
+        {
+            double milky_way = 0;
+            for (int i = 1; i < track.Length; i++)
+            {
+                milky_way += Track(points[track[i]].X, points[track[i]].Y, points[track[i - 1]].X, points[track[i - 1]].Y);
+            }
+            return milky_way;
+        }
+        
+        //проверка повторяющихся последовательностей точек
+        public static bool check_List(List<int[]> arList, int[] track)
+        {
+            bool check = true;
+            for (int i = 0; i < arList.Count; i++)
+            {
+                if (track.SequenceEqual(arList[i]))
+                {
+                    check = false;
+                    break;
+                }
+            }
+            return check;
+        }
+
+        //сравнение P* и случайного числа
+        public static bool checkTemper(double s, double t)
+        {
+            Random rnd = new Random();
+            double prob = 100 * Math.Pow(Math.E, -(s / t));
+            if (prob > rnd.Next(1, 100)) return true;
+            else return false;
+        }
+
+        public static int[] Make_Track(int count)   //мейк трек
+        {
+            int[] track = new int[count + 1];
+            for (int i = 0; i < count; i++)
+                track[i] = i;
+            track[0] = 0;
+            track[track.Length - 1] = track[0];
+            return track;
+        }
+        #endregion
+    }
+}
